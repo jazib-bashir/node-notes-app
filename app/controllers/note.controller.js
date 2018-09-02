0c0917daa50e9b0edec0e508a02dac74a8dab74c
Jazib Bashir <jazibbashir@gmail.com>
2018-09-02 15:47:03 +0500

    create_note - implemented create a new note function with content validation

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -2,7 +2,25 @@ const Note = require('../models/note.model.js');
 
 // Create and Save a new Note
 exports.create = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content cannot be empty"
+        });
+    }
 
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
+    });
+
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
 };
 
 // Retrieve and return all notes from the database.
